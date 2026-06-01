a26a3aef04f19f1bfcf1b5666f3b5e46844b094f
Aswinthraj <femilinaswinthraj731@gmail.com>
2026-06-01 19:08:06 +0530

    Python 22 ex

---
+++ Python/comment.py
@@ -0,0 +1,15 @@
+def calculate_salary():
+
+    #Base of a Salary
+    base_salary=50000
+
+    #bonus_amount
+    bonus=10000
+
+    #calculate total salary
+
+    total_salary=base_salary+bonus
+
+    #Display total Salary
+    print(f"Total Salary : {total_salary}")
+calculate_salary()
