f2e41d756b7bbaabf3d909d4de8b0c416c372488
Aswinthraj <femilinaswinthraj731@gmail.com>
2026-06-01 19:51:52 +0530

    Python 27 ex

---
+++ Python/len.py
@@ -0,0 +1,8 @@
+def length_of_string(text):
+    if text =="":
+        print("Invalid String")
+        return
+    length=len(text)
+    print(f"Length of the String : {length}")
+text="Python"
+length_of_string(text)
